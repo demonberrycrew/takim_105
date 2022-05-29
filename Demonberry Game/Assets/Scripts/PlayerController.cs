1bf5a70d9cd3dc9e3285405215f693b7c7d931d8
Tuğba <kilictugba98@gmail.com>
2022-05-30 00:29:11 +0300

    PlayerController

--- Demonberry Game/Assets/Scripts/PlayerController.cs
+++ Demonberry Game/Assets/Scripts/PlayerController.cs
@@ -4,41 +4,65 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
-    public float jumpForce;
-    public float gravityScale = 5f;
+    public Camera theCamera;
+    public float Speed = 2f;
+    public float SprintSpeed = 5f;
+    public float rotationSpeed = 15f;
+    public float AnimationBlendSpeed = 2f;
+    CharacterController CharController;
+    Animator PlayerAnimator;
 
-    private Vector3 moveDirection;
+    float mDesireRotation = 0f;
+    float mDesireAnimSpeed = 0f;
+    bool mSprinting = false;
 
-    public CharacterController charController;
-
-    private Camera theCamera;
+    float mSpeedY = 0;
+    float mGravity = -9.81f;
 
     // Start is called before the first frame update
     void Start()
     {
-        theCamera = Camera.current;
+        CharController = GetComponent<CharacterController>();
+        PlayerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float yStore = moveDirection.y;
-        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        moveDirection = moveDirection * moveSpeed;
-        moveDirection.y = yStore;
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetButtonDown("Jump"))
+        if (!CharController.isGrounded)
         {
-            moveDirection.y = jumpForce;
+            mSpeedY += mGravity * Time.deltaTime;
+        }
+        else
+        {
+            mSpeedY = 0;
+        }
+   
+        mSprinting = Input.GetKey(KeyCode.LeftShift);
+
+        Vector3 movement = new Vector3(x, 0, z).normalized;
+
+        Vector3 rotatedMovement = Quaternion.Euler(0, theCamera.transform.rotation.eulerAngles.y, 0) * movement;
+        Vector3 verticalMovement = Vector3.up * mSpeedY;
+
+        CharController.Move((verticalMovement + (rotatedMovement * (mSprinting ? SprintSpeed : Speed))) * Time.deltaTime);
+
+        if(rotatedMovement.magnitude > 0)
+        {
+            mDesireRotation = Mathf.Atan2(rotatedMovement.x, rotatedMovement.z) * Mathf.Rad2Deg;
+            mDesireAnimSpeed = mSprinting ? 1 : .5f;
+        }
+        else
+        {
+            mDesireAnimSpeed = 0;
         }
 
-        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
-
-        //transform.position = transform.position + (moveDirection * Time.deltaTime * moveSpeed);
-
-        charController.Move(moveDirection * Time.deltaTime);
-
-        transform.rotation = Quaternion.Euler(0f, theCamera.transform.rotation.eulerAngles.y, 0f);
+        PlayerAnimator.SetFloat("Speed", Mathf.Lerp(PlayerAnimator.GetFloat("Speed"), mDesireAnimSpeed, AnimationBlendSpeed * Time.deltaTime));
+        Quaternion currentRotation = transform.rotation;
+        Quaternion targetRotation = Quaternion.Euler(0, mDesireRotation, 0);
+        transform.rotation = Quaternion.Lerp(currentRotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
