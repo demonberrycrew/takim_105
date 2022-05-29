bf1b3f04bd59cc901532dccc405ea89166249787
Tuğba <kilictugba98@gmail.com>
2022-05-29 22:11:48 +0300

    Fresh start

---
+++ Demonberry Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public float gravityScale = 5f;
+
+    private Vector3 moveDirection;
+
+    public CharacterController charController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yStore = moveDirection.y;
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveDirection = moveDirection * moveSpeed;
+        moveDirection.y = yStore;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+
+        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+
+        //transform.position = transform.position + (moveDirection * Time.deltaTime * moveSpeed);
+
+        charController.Move(moveDirection * Time.deltaTime);
+
+    }
+}
