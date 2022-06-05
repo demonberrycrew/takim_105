e951b4eff46ad03c662d768fa732d00f68328fc8
Tuğba <kilictugba98@gmail.com>
2022-06-05 20:49:39 +0300

    score scene

--- Demonberry Game/Assets/Scripts/FinishLevel.cs
+++ Demonberry Game/Assets/Scripts/FinishLevel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FinishLevel : MonoBehaviour
 {
@@ -11,9 +12,17 @@ public class FinishLevel : MonoBehaviour
     public GameObject timeLeft;
     public GameObject theScore;
     public GameObject totalScore;
+    public int timeCalc;
+    public int scoreCalc;
+    public int totalScored;
 
     void OnTriggerEnter()
     {
+        timeCalc = GlobalTimer.extendScore * 10;
+        timeLeft.GetComponent<Text>().text = "Time Left " + GlobalTimer.extendScore + " x 10";
+        theScore.GetComponent<Text>().text = "Score " + GlobalScore.currentScore;
+        totalScored = GlobalScore.currentScore + timeCalc;
+        totalScore.GetComponent<Text>().text = "Total Score " + totalScored;
         levelMusic.SetActive(false);
         levelTimer.SetActive(false);
         levelComplete.Play();
--- Demonberry Game/Assets/Scripts/GlobalTimer.cs
+++ Demonberry Game/Assets/Scripts/GlobalTimer.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 public class GlobalTimer : MonoBehaviour
 {
     public GameObject timeDisplay01;
-    public GameObject timeDisplay02;
     public bool isTakingTime = false;
-    public int theSeconds = 150;
+    public int theSeconds = 100;
+    public static int extendScore;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +19,9 @@ public class GlobalTimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isTakingTime == false)
+        extendScore = theSeconds;
+
+        if (isTakingTime == false)
         {
             StartCoroutine(SubtractSecond());
         }
@@ -31,7 +33,6 @@ public class GlobalTimer : MonoBehaviour
         isTakingTime = true;
         theSeconds -= 1;
         timeDisplay01.GetComponent<Text>().text = "" + theSeconds;
-        timeDisplay02.GetComponent<Text>().text = "" + theSeconds;
         yield return new WaitForSeconds(1);
         isTakingTime = false;
     }
