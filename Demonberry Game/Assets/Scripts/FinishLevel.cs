9822a77a7eeb9916efef8baf61d60d91329ad2a0
Tuğba <kilictugba98@gmail.com>
2022-06-05 20:25:12 +0300

    gameover + level complete

---
+++ Demonberry Game/Assets/Scripts/FinishLevel.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLevel : MonoBehaviour
+{
+
+    public GameObject levelMusic;
+    public AudioSource levelComplete;
+    public GameObject levelTimer;
+    public GameObject timeLeft;
+    public GameObject theScore;
+    public GameObject totalScore;
+
+    void OnTriggerEnter()
+    {
+        levelMusic.SetActive(false);
+        levelTimer.SetActive(false);
+        levelComplete.Play();
+        StartCoroutine(CalculateScore());
+    }
+
+    IEnumerator CalculateScore()
+    {
+        timeLeft.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        theScore.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        totalScore.SetActive(true);
+    }
+
+}
